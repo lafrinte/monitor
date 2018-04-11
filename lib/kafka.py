daa4973bda696407d0c362d31e1295b0a288bda2
lafrinte <lafrinte@hotmail.com>
2018-04-11 10:28:07 +0800

    Enable rdkafka and normal kafka mode, try rdkafka first and normal kafka as a second choice

--- lib/kafka.py
+++ lib/kafka.py
@@ -10,16 +10,26 @@ from .error import KafkaConnectErr
 
 class KafkaProductor(object):
 
+
     def __init__(self, queues):
         self.queues = queues
         self.kafka = None
         self.topic = None
         self.sessions = None
         self.group = None
+        self.rdkafka = None
         self._init_parse()
+
+    def is_rdkafka(self):
+        try:
+            from pykafka import rdkafka
+            self.rdkafka = True
+        except:
+            self.rdkafka = False
 
     def _init_parse(self):
         self.sessions = [self.is_connection(data['kafka'], data['topic'], data['queue']) for data in self.queues]
+        self.is_rdkafka()
 
     def encode_topic(self, topic):
         if isinstance(topic, bytes) is not True:
@@ -41,14 +51,14 @@ class KafkaProductor(object):
     def start_a_producer(self, session):
 
         _session, _queue = session[0], session[1]
-        with _session.get_producer(delivery_reports=True, use_rdkafka=True) as producer:
+        with _session.get_producer(delivery_reports=True, use_rdkafka=self.rdkafka) as producer:
             count = 0
 
             while True:
                 try:
                     msg = _queue.get_nowait()
                 except:
-                    sleep(0)
+                    self.switch()
                     continue
 
                 count += 1
@@ -58,6 +68,7 @@ class KafkaProductor(object):
                     while True:
                         try:
                             msg, exc = producer.get_delivery_report(block=False)
+                            self.switch()
                             if exc is not None:
                                 print('Failed to deliver msg {}: {}'.format(msg.partition_key, repr(exc)))
                         except:
